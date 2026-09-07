74b2c2f8d74908772f091e1b7f37a431fe8752ff
zhangjinkai123 <13862821822@163.com>
2026-09-07 17:51:15 +0800

    feat: add review result artifact

--- examples/test.java
+++ examples/test.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        String ids = "1,2,3";
         String sql =
             "select * from user where id="
             + ids;
